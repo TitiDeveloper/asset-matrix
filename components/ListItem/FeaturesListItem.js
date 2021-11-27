1fc6d126127927ed5a7f20cf8f09d847397fc1d7
aw3some-dev <olumidesofayo@yahoo.co.uk>
2021-11-27 10:16:30 +0100

    Moh

--- components/ListItem/FeaturesListItem.js
+++ components/ListItem/FeaturesListItem.js
@@ -1,39 +1,59 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { Icon } from '../../constants/Icon';
 
 import PlainText from '../Text/PlainText';
 
-const FeaturesListItem = () => {
-    <View style={{ ...styles.itemContainer, ...props.containerStyle }}>
-        <View>
-            <View style={styles.imageContainer}>
-                <Image source={props.imageSource} />
-            </View>
-            <View>
-                <PlainText>{props.title}</PlainText>
-                <PlainText>{props.description}</PlainText>
-            </View>
+const FeaturesListItem = (props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.8} onPress={props.onTap}>
+      <View style={{ ...styles.itemContainer, ...props.containerStyle }}>
+        <View style={styles.item}>
+          <View style={styles.imageContainer}>
+            <Image source={props.imageSource} />
+          </View>
+
+          <View>
+            <PlainText style={styles.title}>{props.title}</PlainText>
+            <PlainText style={styles.content}>{props.description}</PlainText>
+          </View>
         </View>
 
         <View>
-            <Image source={require('assets/icons/icon.png')} />
+          <Icon name="caret-right" size={12} color="#66737F" />
         </View>
-    </View>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    itemContainer: {
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        backgroundColor: '#ffffff',
-        padding: 10,
-        borderRadius: 10
-    },
-    imageContainer: {
-        borderRadius: 15,
-        marginRight: 7
-    }
+  itemContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 15
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  imageContainer: {
+    borderRadius: 15,
+    marginRight: 7
+  },
+  title: {
+    fontWeight: '700',
+    color: '#272D37'
+  },
+  content: {
+    fontSize: 12,
+    color: '#66737F'
+  }
 });
 
 export default FeaturesListItem;
---
+++ constants/Icon.js
@@ -0,0 +1,7 @@
+import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+
+export const Icon = createIconSetFromIcoMoon(
+    require('assets/resources/selection.json'),
+    'IcoMoon',
+    'icomoon.ttf'
+);
